043e30052f15c9700f615404391a71d586e6aa0b
matonny <onny.mat@gmail.com>
2020-06-07 21:44:16 +0200

    drawing tetrominos

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ struct tetromino {
     int color;
 };
 enum colors  {
-    cyan = 0,
-    blue = 1,
-    white = 2,
-    yellow = 3,
-    green = 4,
-    magenta = 5,
-    red = 6
+    cyan = 1,
+    blue = 2,
+    white = 3,
+    yellow = 4,
+    green = 5,
+    magenta = 6,
+    red = 7
 };
 const tetromino shapeI = {
         {{0,0}, {0,1}, {0, 2}, {0,3}},
@@ -47,8 +47,29 @@ const tetromino shapeZ = {
         colors(red)
 };
 using table = std::vector< tetromino >;
-
+void drawTetromino (const tetromino &drawnTetromino);
 int main()
 {
-
+    tetromino currentTetromino = shapeI;
+    initscr();
+    curs_set(0);
+    start_color();
+    init_pair(1, COLOR_CYAN, COLOR_CYAN);
+    init_pair(colors(blue), COLOR_BLUE, COLOR_BLUE);
+    init_pair(colors(white), COLOR_WHITE, COLOR_WHITE);
+    init_pair(colors(yellow), COLOR_YELLOW, COLOR_YELLOW);
+    init_pair(colors(green), COLOR_GREEN, COLOR_GREEN);
+    init_pair(colors(magenta), COLOR_MAGENTA, COLOR_MAGENTA);
+    init_pair(colors(red), COLOR_RED, COLOR_RED);
+    drawTetromino(currentTetromino);
+    getch();
+    endwin();
+}
+void drawTetromino(const tetromino &drawnTetromino){
+    attron(COLOR_PAIR(drawnTetromino.color));
+    for(int i = 0; i < drawnTetromino.blocks.size(); i++){
+        mvprintw(drawnTetromino.blocks[i].y, drawnTetromino.blocks[i].x, "*");
+    }
+    attroff(COLOR_PAIR(drawnTetromino.color));
+    refresh();
 }
